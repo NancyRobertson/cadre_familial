6e777befbc44c96c4f913841374c4172c93ae8f7
NancyRobertson <32757401+NancyRobertson@users.noreply.github.com>
2017-10-15 23:07:24 -0400

    move function save_and_upload up

--- src/backup_rdb.py
+++ src/backup_rdb.py
@@ -13,33 +13,9 @@ import argparse
 import shutil
 import redis
 
-# Get your app key and secret from the Dropbox developer website
-app_key = os.environ['CRON_DROP_KEY']
-app_secret = os.environ['CRON_DROP_SECRET']
-
-parser = argparse.ArgumentParser(
-    description='Lis les messages et uploade les fichiers attaches a dropbox.')
-parser.add_argument('--version', '-V', action='version', version='%(prog)s 0.1')
-parser.add_argument('--dbx_token', '-t', action='store', 
-    help='the access_token for a known dropbox account.')
-parser.add_argument('--dbx_folder', '-f', action='store', 
-    default='/redis_data',
-    help='the dropbox folder to store redis file.')
-parser.add_argument('--local_folder','-l', action='store', 
-    default='/data',
-    help='the local folder to store attachments.')
-parser.add_argument('--local_only', '-o', action='store_true',
-    help='do not upload the pictures to dropbox')
-parser.add_argument('--no_metadata', '-M', action='store_true',
-    help='do not store metadata in redis')    
-parser.add_argument('--verbose', '-v', action='count',
-    help='increase verbosity')
-
-args = parser.parse_args()
-if args.verbose >= 2:
-    print "Verbosity level: %d " % (args.verbose)
-save_and_upload()
-
+###
+# Trigger redis to export its datastore
+# And upload it to the dropbox account
 def save_and_upload() :
     filename = 'dump.rdb'
 
@@ -68,3 +44,34 @@ def save_and_upload() :
             return False
 
     return True
+
+##########################################
+# Main
+
+# Get your app key and secret from the Dropbox developer website
+app_key = os.environ['CRON_DROP_KEY']
+app_secret = os.environ['CRON_DROP_SECRET']
+
+parser = argparse.ArgumentParser(
+    description='Lis les messages et uploade les fichiers attaches a dropbox.')
+parser.add_argument('--version', '-V', action='version', version='%(prog)s 0.1')
+parser.add_argument('--dbx_token', '-t', action='store', 
+    help='the access_token for a known dropbox account.')
+parser.add_argument('--dbx_folder', '-f', action='store', 
+    default='/redis_data',
+    help='the dropbox folder to store redis file.')
+parser.add_argument('--local_folder','-l', action='store', 
+    default='/data',
+    help='the local folder to store attachments.')
+parser.add_argument('--local_only', '-o', action='store_true',
+    help='do not upload the pictures to dropbox')
+parser.add_argument('--no_metadata', '-M', action='store_true',
+    help='do not store metadata in redis')    
+parser.add_argument('--verbose', '-v', action='count',
+    help='increase verbosity')
+
+args = parser.parse_args()
+if args.verbose >= 2:
+    print "Verbosity level: %d " % (args.verbose)
+save_and_upload()
+
